b64962fd3a04062b347c1ffb58aaae42c040c1b4
amisha715 <mauryaamisha731@gmail.com>
2025-09-13 16:44:43 +0530

    Multi-Client TCP Chat System

---
+++ client.py
@@ -0,0 +1,39 @@
+import threading
+import socket
+
+alias = input('Choose an alias >>> ')
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 59000))
+
+
+def client_receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message == "alias?":
+                client.send(alias.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print('Error! Connection closed.')
+            client.close()
+            break
+
+
+def client_send():
+    while True:
+        try:
+            message = f'{alias}: {input("")}'
+            client.send(message.encode('utf-8'))
+        except:
+            print("Error sending message.")
+            client.close()
+            break
+
+# Start threads
+
+receive_thread = threading.Thread(target=client_receive)
+receive_thread.start()
+
+send_thread = threading.Thread(target=client_send)
+send_thread.start()
